661a064246bf1d79c16ce02a2103bef25cb11b83
benflo <benji@DESKTOP-JQVMOHH.>
2025-03-11 14:56:21 +0100

    simple database functions and blueprint

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,9 @@
 #SQL-script för att skapa tabeller
+
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT, -- unique user ID
+	name TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL,
+	balance REAL DEFAULT 0.0 -- user balance
+);
