11cdc497cd0657d66598a0e5944d625110258808
benflo <benji@DESKTOP-JQVMOHH.>
2025-03-11 16:47:26 +0100

    fix: return statements in auth.py

--- services/auth.py
+++ services/auth.py
@@ -3,27 +3,22 @@ from db.database import add_user, get_user_by_email, delete_user
 
 def register_user(name, email, password):
     if get_user_by_email(email):
-        print("email already in use")
+        return False
     else:
         add_user(name,email,password)
+	return True
 
 def remove_user(email, userid):
     if get_user_by_email(email):
         delete_user(userid)
+        return True
     else:
-        print("No user with that email exists")
+        return False
 
-def user_loggin(email,password):
+def user_login(email,password):
     user = get_user_by_email(email)
     if user[3] == password:
         return True
     else:
-        print("No user with that email exists")
+        return False
 
-
-
-
-
-
-
-    
